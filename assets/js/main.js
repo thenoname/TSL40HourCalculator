ced79f89970cf1e138801ce1324910ba5211e743
thenoname <Thenoname@gmail.com>
2022-12-13 11:42:49 -0800

    Complete revamp to allow user to enter pay by date

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 document.addEventListener('DOMContentLoaded', function(){
     // Hide the form
     document.getElementById('infoForm').setAttribute('hidden','');
@@ -10,90 +12,206 @@ document.addEventListener('DOMContentLoaded', function(){
         }
     });
     
-    let submitButton = document.getElementById('submitButton');
-    submitButton.addEventListener('click', calcPay)
+    let addRowButton = document.getElementById('addRowButton');
+    addRowButton.addEventListener('click', onAddRowClicked);
+    
+    // Set start date and end date min/max
+    document.getElementById('startDate').setAttribute('min','2021-11-01');    
+    document.getElementById('endDate').setAttribute('min','2021-11-01');
    
 });
 
-function calcPay() {
-    currHourlyPay = document.getElementById('currHourlyPay').value;
-    category = document.getElementById('category').value;
-    hoursTypicallyWorked = document.getElementById('hoursTypicallyWorked').value;
-    weeks2021 = document.getElementById('weeksLastYear').value;
-    weeks2022 = document.getElementById('weeksThisYear').value;
-    
-    if(currHourlyPay==""||category=="0"||hoursTypicallyWorked==""||weeksLastYear==""||weeksThisYear==""){
-        return;
+// Explicit Globals
+var nov2021Rate = 0;
+var nov2022Rate = 0;
+var nov2023Rate = 0;
+var nov2021Str = '2021-11-01';
+var nov2022Str = '2022-11-01';
+var nov2023Str = '2023-11-01';
+var nov2021Date = new Date(nov2021Str);
+var nov2022Date = new Date(nov2022Str);
+var nov2023Date = new Date(nov2023Str);
+var nov2021Time = nov2021Date.getTime()/1000;
+var nov2022Time = nov2022Date.getTime()/1000;
+var nov2023Time = nov2023Date.getTime()/1000;
+var nov2021RateFound = false;
+var minimum = 0;
+var startDate = '';
+var endDate = '';
+var startTime = 0;
+var endTime = 0;
+var hourlyRate = 0;
+var category = 0;
+var categoryObj = '';
+var hoursWorked = 0;
+function onAddRowClicked() {
+    if(checkInput()){
+        // Case 1 - Entire entered time is before 11/1/2022
+        if (startTime >= nov2021Time && startTime < nov2022Time && endTime >= nov2021Time && endTime < nov2022Time){            
+            storeInTable(startDate, endDate, hourlyRate, minimum, category, hoursWorked);
+        }else if(startTime >= nov2022Time){ //Case 2 - Entire entered time is after 11/1/2022
+            calculateTransitionRates(hourlyRate);
+            storeInTable(startDate, endDate, hourlyRate, nov2022Rate, category, hoursWorked);
+        }else if(startTime >= nov2021Time && startTime < nov2022Time && endTime >= nov2022Time){ //Case 3 - Part of entered time is after 11/1/2022
+            calculateTransitionRates(hourlyRate);
+            // Write a row for the pre transition to 42
+            storeInTable(startDate, nov2022Str, hourlyRate, minimum, category, hoursWorked);
+            // Write a row for the post transition to 42
+            storeInTable(nov2022Str, endDate, hourlyRate, nov2022Rate, category, hoursWorked);
+        }
     }
-    currHourlyPay = parseFloat(currHourlyPay);
-    hoursTypicallyWorked = parseFloat(hoursTypicallyWorked);
-    weeks2021 = parseFloat(weeks2021);
-    weeks2022 = parseFloat(weeks2022);
+}
+
+function calculateTransitionRates(hourlyRate){
+    if(hourlyRate>minimum){
+        nov2021Rate = hourlyRate; //GLOBAL
+    }else{
+        nov2021Rate = minimum; //GLOBAL
+    }
+    nov2022Rate = nov2021Rate*1.1047; //GLOBAL
+    nov2023Rate = nov2022Rate*1.075; //GLOBAL
+
+    nov2022Rate = parseFloat(nov2022Rate.toFixed(2)); //GLOBAL
+    nov2023Rate = parseFloat(nov2023Rate.toFixed(2)); //GLOBAL
+
+    document.getElementById('rate2021').value = nov2021Rate;
+    document.getElementById('rate2022').value = nov2022Rate;
+    document.getElementById('rate2023').value = nov2023Rate;
+}
+
+function checkInput() {
+    hourlyRate = parseFloat(document.getElementById('hourlyRate').value);//GLOBAL
+    categoryObj = document.getElementById('category');//GLOBAL
+    category = categoryObj.value;//GLOBAL
+    hoursWorked = parseFloat(document.getElementById('hoursWorked').value);//GLOBAL
+    startDate = document.getElementById('startDate').value;//GLOBAL
+    endDate = document.getElementById('endDate').value;//GLOBAL
     
-    rate2021 = 0;
+    if(hourlyRate==""||category=="0"||hoursWorked==""||isNaN(hoursWorked)||startDate==""||endDate==""){
+        return false;
+    }    
+    // Refresh global variables if needed
+    minimum = getMinimum(category); //GLOBAL
+    startTime = new Date(startDate).getTime()/1000; //GLOBAL
+    endTime = new Date(endDate).getTime()/1000; //GLOBAL
+    if(startTime < nov2021Time || endTime < nov2021Time){
+        alert("Dates must be after Nov 1, 2021");
+        return false;
+    }
+    return true;
+}
+
+function storeInTable(startDate, endDate, hourlyRatePaid, hourlyRateOwed, category, hoursWorked) {
+    // Do calculations    
+    calculateTransitionRates();
+    var numWeeks = getWeeks(startDate, endDate);
+    var backpay = calcBackPay(numWeeks, hourlyRatePaid, hourlyRateOwed, hoursWorked);
+
+    // Get fields and add row
+    var table = document.getElementById("payTable");
+    var numRows = table.rows.length;
+    var row = table.insertRow(numRows);
+    var row_startDate = row.insertCell(0);
+    var row_endDate = row.insertCell(1);
+    var row_hourlyRatePaid = row.insertCell(2);
+    var row_hourlyRateOwed = row.insertCell(3);
+    var row_category = row.insertCell(4);
+    var row_hoursWorked = row.insertCell(5);
+    var row_minimum = row.insertCell(6);
+    var row_backPay = row.insertCell(7);
+    
+    row_startDate.innerHTML = startDate;
+    row_endDate.innerHTML = endDate;
+    row_category.innerHTML = categoryObj.options[parseInt(category)].text;
+    row_hoursWorked.innerHTML = hoursWorked;
+    row_minimum.innerHTML = minimum;
+    row_backPay.innerHTML = backpay
+    row_hourlyRatePaid.innerHTML = hourlyRatePaid; 
+    row_hourlyRateOwed.innerHTML = hourlyRateOwed; 
+}
+
+function getWeeks(date1, date2){
+    date1 = new Date(date1);
+    date2 = new Date(date2);    
+    var diffTime = (date2.getTime() - date1.getTime()) / 1000;
+    // Make sure that the second date is after the first date
+    if(diffTime < 0){
+        diffTime = diffTime * -1;
+    }
+    diffTime = diffTime  / (60 * 60 * 24 * 7);
+    
+    // Check if we can get what Nov 2021 was
+    getNov2021Rate(date1, date2);
+    
+    return diffTime;
+}
+
+function getNov2021Rate(date1, date2){
+    var date1Time = date1.getTime()/1000;
+    var date2Time = date2.getTime()/1000;
+    if(nov2021Time >= date1Time && nov2021Time <= date2Time){
+        if(hourlyRate<minimum){
+            nov2021Rate = minimum; //GLOBAL
+        }else{            
+            nov2021Rate = hourlyRate; //GLOBAL
+        }
+        nov2021RateFound = true; //GLOBAL
+    }    
+}
+
+function getMinimum(category) {
+    var minimum = 0;
     switch (category){
         case "1":
-            rate2021 = 53.15;
+            minimum = 53.15;
             break;
         case "2":
-            rate2021 = 45.48;
+            minimum = 45.48;
             break;
         case "3":
-            rate2021 = 49.30;
+            minimum = 49.30;
             break;
         case "4":
-            rate2021 = 38.63;
+            minimum = 38.63;
             break;
         case "5":
-            rate2021 = 29.00;
+            minimum = 29.00;
             break;
         case "6":
-            rate2021 = 32.38;
+            minimum = 32.38;
             break;
         case "7":
-            rate2021 = 35.78;
+            minimum = 35.78;
             break;
         case "8":
-            rate2021 = 34.40;
+            minimum = 34.40;
             break;
         case "9":
-            rate2021 = 36.06;
+            minimum = 36.06;
             break;
         case "10":
-            rate2021 = 37.73;
+            minimum = 37.73;
             break;
     }
-    
-    if(currHourlyPay>=rate2021){
-        rate2021=currHourlyPay;
+    return minimum;
+}
+
+function calcBackPay(weeks, hourlyRatePaid, hourlyRateOwed, hoursWorked) {
+    // If making more than owed, no back pay
+    if(hourlyRatePaid>hourlyRateOwed){
+        return 0;
     }
-            
-    rate2022 = rate2021*1.1047;
-    rate2023 = rate2022*1.075;
-    
-    rate2022 = parseFloat(rate2022.toFixed(2));
-    rate2023 = parseFloat(rate2023.toFixed(2));
-    
-    straightTime = 0;
-    overTime = 0;
-    if(hoursTypicallyWorked>=40){
+    var straightTime = hoursWorked;
+    var overTime = 0;
+    if(hoursWorked>40){
         straightTime = 40;
-        overTime = hoursTypicallyWorked - 40;
-    }else{
-        straightTime = hoursTypicallyWorked;
-        overTime = 0;
+        overTime = hoursWorked - 40;
     }
-    
-    backPay2021 = ((rate2021-currHourlyPay)*straightTime + (rate2021-currHourlyPay)*overTime*1.5)*weeks2021;
-    backPay2022 = ((rate2022-currHourlyPay)*straightTime + (rate2022-currHourlyPay)*overTime*1.5)*weeks2022;
-    
-    backPay2021 = backPay2021.toFixed(2);
-    backPay2022 = backPay2022.toFixed(2);
-    
-    document.getElementById('rate2021').value = rate2021;
-    document.getElementById('rate2022').value = rate2022;
-    document.getElementById('rate2023').value = rate2023;
-    document.getElementById('backpay2021').value = backPay2021;
-    document.getElementById('backpay2022').value = backPay2022;
+    var hourlyDiff = hourlyRateOwed - hourlyRatePaid;
+    var weeklyDiff = hourlyDiff*straightTime + hourlyDiff*overTime*1.5;
+    var backpay = weeklyDiff*weeks;
+    backpay = parseFloat(backpay.toFixed(2));
+
+    return backpay;
     
 }
